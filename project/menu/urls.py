059c56d38af8fb129d7cb83def74f9c588696a56
Ivan <vanechka.nikitin.20044@gmail.com>
2024-07-26 19:12:10 +0800

    Add template for testings and update design

--- project/menu/urls.py
+++ project/menu/urls.py
@@ -5,4 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('services/web-development/', views.web_development, name='web_development'),
+    path('services/seo/', views.seo, name='seo'),
 ]
--- project/menu/views.py
+++ project/menu/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def home(request):
+    return render(request, 'home.html', {'title': 'Home'})
+
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About'})
+
+
+def services(request):
+    return render(request, 'services.html', {'title': 'Services'})
+
+
+def web_development(request):
+    return render(request, 'web_development.html', {'title': 'Web Development'})
+
+
+def seo(request):
+    return render(request, 'seo.html', {'title': 'SEO'})
