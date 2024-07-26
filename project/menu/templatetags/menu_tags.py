a2d2fb07790fadb311a400bf623bcdaf79dd0633
Ivan <vanechka.nikitin.20044@gmail.com>
2024-07-26 17:59:18 +0800

    Create logic for tree menu

---
+++ project/menu/templatetags/menu_tags.py
@@ -0,0 +1,38 @@
+from django import template
+from django.urls import resolve, Resolver404
+
+from ..models import Menu, MenuItem
+
+register = template.Library()
+
+
+@register.inclusion_tag('menu/draw_menu.html', takes_context=True)
+def draw_menu(context, menu_name):
+    try:
+        menu = Menu.objects.get(name=menu_name)
+    except Menu.DoesNotExist:
+        return {'menu': None}
+
+    try:
+        current_url = resolve(context['request'].path_info).url_name
+    except Resolver404:
+        current_url = None
+
+    def build_tree(items, parent=None):
+        tree = []
+        for item in items:
+            if item.parent == parent:
+                tree.append({
+                    'item': item,
+                    'children': build_tree(items, item)
+                })
+        return tree
+
+    items = MenuItem.objects.filter(menu=menu).select_related('parent').order_by('order')
+    menu_tree = build_tree(items)
+
+    return {
+        'menu': menu,
+        'menu_tree': menu_tree,
+        'current_url': current_url
+    }
